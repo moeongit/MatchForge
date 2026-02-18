5c4515c976cd71df83f748c65840f2d47c781fa0
MoeOnGit <malrobiay04@gmail.com>
2026-02-18 04:04:00 -0700

    rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: "/actuator/:path*", destination: "/api/actuator/:path*" },
+      { source: "/players", destination: "/api/players" },
+      { source: "/queue/:path*", destination: "/api/queue/:path*" },
+      { source: "/queues", destination: "/api/queues" },
+      { source: "/queues/:path*", destination: "/api/queues/:path*" },
+      { source: "/matchmake/:path*", destination: "/api/matchmake/:path*" },
+      { source: "/matches/:path*", destination: "/api/matches/:path*" }
+    ];
+  }
 };
 
 module.exports = nextConfig;
